0b3c70b7e4c2ea79a0f0132b694c96f80a0ae3ec
Igor Westphal <westphaligor2@gmail.com>
2024-04-18 13:33:50 +0200

    Setzt Arbeit an edit_button_page fort

--- features/Pages/edit_button_page.py
+++ features/Pages/edit_button_page.py
@@ -6,3 +6,17 @@ class EditButton(BasePage):
     def __init__(self, context):
         BasePage.__init__(self, context.driver)
         self.libs = Library()
+        "/html/body/div[4]/table/tbody/tr[4]/td[2]"
+        "/html/body/div[4]/table/tbody/tr[4]/td[3]"
+        "/html/body/div[4]/table/tbody/tr[4]/td[4]/i"
+        "/html/body/div[5]/div[1]/h2"
+        "/html/body/div[5]/div[1]/label[1]"
+        "//*[@id='aspect']"
+        "/html/body/div[5]/div[1]/label[2]"
+        "//*[@id='value']"
+        "/html/body/div[5]/div[2]/button[1]"
+        "/html/body/div[5]/div[2]/button[2]"
+    
+    def click_on_edit_button(self):
+        # get the textcontent of the row before click the button in this row
+        pass     
