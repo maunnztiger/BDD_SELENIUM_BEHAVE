cb0dd152a04227e70058ca50224fb04e08acc527
Igor Westphal <westphaligor2@gmail.com>
2024-06-18 17:23:45 +0200

    Setzt Tests zum slideshow feature fort:wq

--- features/Pages/slide_show_page.py
+++ features/Pages/slide_show_page.py
@@ -13,21 +13,27 @@ class SlideShowPage(BasePage):
         self.libs = Library()
         self.driver = context.driver
         self.menu_button_xpath = "//*[@id='menuButton']"
-        self.menu_point_link_text = "Slideshows"
         self.page_link_xpath = "/html/body/div/a"
-        self.pages_url = "http://www.dexterslab.com:8080/slide_1.html"
         self.slide_show_document_title_xpath = "/html/head/meta[1]"
-        self.imgae_xpath = "/html/body/div[2]/div[2]/div/div/div/div/div[3]/svg/g/g/g/image"
-           
+        self.slide_show_first_image_xpath = "//image[@clip-path='url(#p.1)']"
+        self.slide_no_field_xpath = "//*[@id=':l']"
+        self.progress_slide_up_button_xpath = "/html/body/div[2]/div[1]/div[1]/div[3]"
+        self.progress_slide_down_button_xpath = "/html/body/div[2]/div[1]/div[1]/div[1]"
+              
     def open_frontpage_menu(self):
         element = self.libs.get_element_by_xpath(self.driver, self.menu_button_xpath)
         element.click()
         sleep(1)
     
     def click_menu_point(self, menu_point):
-        element =self.libs.get_element_by_linktext(self.driver, self.menu_point_link_text)
+        element = self.libs.get_element_by_linktext(self.driver, menu_point)
         element.click()
         sleep(1)
+    
+    def clicking_link(self):
+        element = self.libs.get_element_by_xpath(self.driver, self.page_link_xpath)
+        element.click()
+        sleep(1)            
     
     def verify_pages_linkt_text(self, link_text):
         element = self.libs.get_element_by_xpath(self.driver, self.page_link_xpath)
@@ -38,10 +44,54 @@ class SlideShowPage(BasePage):
     def verify_slide_show_pages_url(self, slide_show_page_url):
         url = self.driver.current_url 
         assert url == slide_show_page_url
-        
+        sleep(1)
+       
     def verify_first_aria_label(self, first_aria_label):
-        element = self.libs.get_element_by_xpath(self.driver, self.imgae_xpath)
-        image_label = element.get_attribute('aria-label')
+        # WebDriverWait(self.driver, 5).until(EC.frame_to_be_available_and_switch_to_it((By.XPATH, "/html/body/div/iframe")))
+        frame1 = self.driver.find_element(by=By.XPATH, value='/html/body/div/iframe')
+        self.driver.switch_to.frame(frame1)
+        sleep(2)
+        image =self.driver.find_element(by=By.TAG_NAME, value='image' )
+        image_label = image.get_attribute('aria-label')
         assert first_aria_label == image_label
+        sleep(1)
     
-                  
+    def verify_first_slide_page_number(self, first_page_number):
+        slide_no_field = self.libs.get_element_by_xpath(self.driver, self.slide_no_field_xpath)
+        slide_no = slide_no_field.text
+        print(slide_no, first_page_number)
+        assert slide_no == first_page_number
+
+    def click_the_right_arrow(self):
+        element = self.libs.get_element_by_xpath(self.driver, "//div[@class='punch-viewer-navbar-next goog-inline-block goog-flat-button']")
+        element.click()
+        sleep(1)
+    
+    def verify_second_aria_label(self, second_aria_label):
+        image = self.driver.find_element(by=By.TAG_NAME, value='image' )
+        image_label = image.get_attribute('aria-label')
+        assert image_label == second_aria_label
+        sleep(1)
+    
+    def veryify_second_slide_page_number(self, second_page_number): 
+        slide_no_field = self.libs.get_element_by_xpath(self.driver, self.slide_no_field_xpath)
+        slide_no = slide_no_field.text
+        assert slide_no == second_page_number
+        sleep(1)
+        
+    def click_left_arrow_button(self):
+        button = self.libs.get_element_by_xpath(self.driver, "//div[@class='punch-viewer-navbar-prev goog-inline-block goog-flat-button']")
+        button.click()
+        sleep(1)
+    
+    def verify_first_slide_page_reload(self, first_aria_label):
+        image = self.driver.find_element(by=By.TAG_NAME, value='image' )
+        image_label = image.get_attribute('aria-label')
+        assert image_label == first_aria_label
+        sleep(1)        
+        
+    def verify_first_slide_page_number(self, first_page_number):
+        slide_no_field = self.libs.get_element_by_xpath(self.driver, self.slide_no_field_xpath)
+        slide_no = slide_no_field.text
+        assert slide_no == first_page_number
+             
--- features/steps/slide_show.py
+++ features/steps/slide_show.py
@@ -24,23 +24,19 @@ def step_impl(context, slide_show_page_url):
 def step_impl(context, first_aria_label):
     context.slide_show.verify_first_aria_label(first_aria_label)
         
-@step('the page has the number one "{page_number}"')
-def step_impl(context, page_number):
-    context.slide_show.verify_first_slide_page_number(page_number)        
+@step('the page has the number "{first_page_number}"')
+def step_impl(context, first_page_number):
+    context.slide_show.verify_first_slide_page_number(first_page_number)        
     
 @when('the user clicks on the right-arrow on the right of the number 1')
 def step_impl(context):
     context.slide_show.click_the_right_arrow()    
     
-@then('a new slide-page with another image is loaded')
-def step_impl(context):
-    context.slide_show.verify_new_slide_page_loaded()
-
-@step('the new image has the aria-label "{second_aria_label}"')    
+@then('a new slide-page with new image has the aria-label "{second_aria_label}"')
 def step_impl(context, second_aria_label):
     context.slide_show.verify_second_aria_label(second_aria_label)
-    
-@step('the page has the number "{second_page_number}"')
+
+@step('the page shows number "{second_page_number}"')
 def step_impl(context, second_page_number):
     context.slide_show.veryify_second_slide_page_number(second_page_number)        
     
@@ -48,10 +44,10 @@ def step_impl(context, second_page_number):
 def step_impl(context):
     context.slide_show.click_left_arrow_button()
 
-@then('the first page with the first image is loaded again')
-def step_impl(context):
-    context.slide_show.verify_first_slide_page_reload()    
+@then('the first page with the first image with the aria-label "{first_aria_label}" is loaded again')
+def step_impl(context, first_aria_label):
+    context.slide_show.verify_first_slide_page_reload(first_aria_label)    
     
-@step('the first page has the number "{page_number}"')        
-def step_impl(context, page_number):
-    context.slide_show.verify_first_slide_page_number(page_number)
+@step('the first page shows the number "{first_page_number}"')        
+def step_impl(context, first_page_number):
+    context.slide_show.verify_first_slide_page_number(first_page_number)
