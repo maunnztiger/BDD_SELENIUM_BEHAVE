39ab17fb0da2397673e77367f2e60961db0815ec
Igor Westphal <westphaligor2@gmail.com>
2024-07-16 14:25:56 +0200

    Ergänzt run_tests.sh um einen weiteren feature-run

--- run_tests.sh
+++ run_tests.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 source ./venv/bin/activate
-sleep 2
-behave ./features/basic_menu.feature ./features/create_button.feature ./features/edit_button.feature ./features/return_button.feature ./features/table_content.feature ./features/video_element.feature
+sleep 1
+behave ./features/basic_menu.feature ./features/create_button.feature ./features/edit_button.feature ./features/return_button.feature ./features/table_content.feature ./features/video_element.feature  ./features/slide_show.feature -f allure_behave.formatter:AllureFo>sleep 1
+allure generate Report_Json -o /home/igor/monitoring_tool/Report_Html --clean
+
