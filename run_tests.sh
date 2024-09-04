cc1187bc8394325644b4cbdfd4dded5202acbb21
Igor Westphal <westphaligor2@gmail.com>
2024-09-04 14:04:35 +0200

    Fügt neuen Test zum Shellscript hinzu

--- run_tests.sh
+++ run_tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 source ./venv/bin/activate
 sleep 1
-behave ./features/basic_menu.feature ./features/create_button.feature ./features/edit_button.feature ./features/return_button.feature ./features/table_content.feature ./features/video_element.feature -f allure_behave.formatter:AllureFormatter -o Report_Json
+behave ./features/basic_menu.feature ./features/create_button.feature ./features/edit_button.feature ./features/return_button.feature ./features/table_content.feature ./features/video_element.feature ./features/text_resources.feature -f allure_behave.formatter:AllureFormatter -o Report_Json
 sleep 1
 allure generate Report_Json -o /home/igor/monitoring_tool/Report_Html --clean
 
